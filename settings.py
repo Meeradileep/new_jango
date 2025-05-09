81dfbccf9334cb060d17d7a8b2b87d4308bdb868
Meeradileep <148002709+Meeradileep@users.noreply.github.com>
2025-05-09 11:59:38 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
-
+pip install dj-database-url
+import dj-database-url
+import os
+DATABASES = {
+ "default": dj_database_url.parse(os.environ.get("postgresql://test_db_864t_user:VcgNwoa8ZhPVv6P8PjnYwukHcsBLx1fs@dpg-d0epfd6mcj7s7382mqjg-a.oregon-postgres.render.com/test_db_864t"))
+}
